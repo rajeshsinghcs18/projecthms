fc7b7ec57f6eb4834b148f8475d4136164c16443
Rajesh Singh <rajesh.singh_cs18>
2021-05-27 11:37:48 +0530

    added extra required jars and created login class , and make connection for database

---
+++ src/hotel/management/system/conn.java
@@ -0,0 +1,20 @@
+package hotel.management.system;
+
+import java.sql.*;
+class conn {
+
+    Connection c;
+    Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/HotelManage?serverTimezone=UTC","root","kutta123");
+
+            s =c.createStatement();
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
